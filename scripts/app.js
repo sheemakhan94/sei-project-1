297dd2b4dc0e924e0c2727999aa4defe8543f411
Sheema <sa_khan94@hotmail.co.uk>
2019-06-03 16:10:40 +0100

    collision detection in first row

--- scripts/app.js
+++ scripts/app.js
@@ -5,31 +5,61 @@ const height = 9
 const squares = []
 let playerIndex = 110
 let enemyIndex = 0
-
+let cashIndex = null
 
 function movePlayer() {
   squares.forEach(square => square.classList.remove('player'))
   squares[playerIndex].classList.add('player')
 }
 
-function moveEnemy() {
-  squares.forEach(square => square.classList.remove('enemy'))
-  for (let i = 0; i < 5; i++) {
-    squares[enemyIndex+i].classList.add('enemy')
-    squares[enemyIndex+i + width].classList.add('enemy')
-    squares[enemyIndex+i + (width * 2)].classList.add('enemy')
+function handleKeyDown(e) {
+  let playerShouldMove = true
+  let cashShouldFire = false
+  switch(e.keyCode) {
+    case 39:
+      if (playerIndex % width < width - 1) {
+        playerIndex++
+      }
+      break
+    case 37:
+      if (playerIndex % width > 0) {
+        playerIndex--
+      }
+      break
+    case 32:
+      cashShouldFire = true
+      cashIndex = playerIndex - width
+      break
+    default:
+      playerShouldMove = false
+      cashShouldFire = false
   }
+  if (playerShouldMove) movePlayer()
+  if (cashShouldFire) shootCash()
 }
 
+function moveEnemy() {
+  squares.forEach(square => square.classList.remove('enemy'))
+  squares.forEach(square => square.dataset.id = null)
+  for (let i = 0; i < 7; i++) {
+    if (enemies[i]) {
+      squares[enemyIndex+i].classList.add('enemy')
+      squares[enemyIndex+i].dataset.id  = i
+    }
+
+    // squares[enemyIndex+i + width].classList.add('enemy')
+    // squares[enemyIndex+i + width].dataset.id  = i + 7
+    // squares[enemyIndex+i + (width * 2)].classList.add('enemy')
+    // squares[enemyIndex+i + width].dataset.id  = i + 14
+  }
+}
 
 
 let count = 0
 let movingRight = true
 
 function move() {
-
-
-  if(count < 8) {
+  if(count < 6) {
     // console.log(count)
     count++
     if (movingRight) {
@@ -48,27 +78,42 @@ function move() {
 }
 setInterval(move, 500)
 
+let cashTimer = null
 
 
-function handleKeyDown(e) {
-  let playerShouldMove = true
-  switch(e.keyCode) {
-    case 39:
-      if (playerIndex % width < width - 1) {
-        playerIndex++
-      }
-      break
-    case 37:
-      if (playerIndex % width > 0) {
-        playerIndex--
-      }
-      break
-    default:
-      playerShouldMove = false
+function moveCash() {
+  checkHits()
+  squares.forEach(square => square.classList.remove('cash'))
+  cashIndex -= width
+  if (squares[cashIndex]) {
+    squares[cashIndex].classList.add('cash')
+  } else {
+    clearInterval(cashTimer)
   }
-  if (playerShouldMove) movePlayer()
 }
 
+function shootCash() {
+  squares[cashIndex].classList.add('cash')
+  cashTimer = setInterval(moveCash, 500)
+  // don't forget to put a timeout in here as well or the missile will run eternally!
+  setTimeout(() => {
+    // console.log('clear the cash interval')
+    clearInterval(cashTimer)
+  }, 4000)
+
+}
+
+function checkHits() {
+  squares.forEach(square => {
+    if(square.classList.contains('cash') && square.classList.contains('enemy')) { // turn the class off and clear moveCash interval
+      enemies[parseInt(square.dataset.id)] = false
+      square.classList.remove('enemy')
+    }
+  })
+  console.log(enemies)
+}
+
+let enemies = []
 
 function init() {
 
@@ -82,11 +127,17 @@ function init() {
     grid.append(square)
   }
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 7; i++) {
     squares[enemyIndex+i].classList.add('enemy')
-    squares[enemyIndex+i + width].classList.add('enemy')
-    squares[enemyIndex+i + (width * 2)].classList.add('enemy')
+    squares[enemyIndex+i].dataset.id  = i
+    // squares[enemyIndex+i + width].classList.add('enemy')
+    // squares[enemyIndex+i + width].dataset.id  = i + 7
+    // squares[enemyIndex+i + (width * 2)].classList.add('enemy')
+    // squares[enemyIndex+i + width].dataset.id  = i + 14
+    enemies.push(true)
+    console.log(enemies)
   }
   squares[playerIndex].classList.add('player')
+  // squares[cashIndex].classList.add('cash')
   window.addEventListener('keydown', handleKeyDown)
 }
